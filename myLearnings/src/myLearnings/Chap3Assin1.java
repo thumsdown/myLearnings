495582d7712b49c937b9bde18a85f15cbf4533cf
Vinit <thumsdown@gmail.com>
2016-01-23 09:30:45 -0800

    JAVA:Chap3 Assignment1

---
+++ myLearnings/src/myLearnings/Chap3Assin1.java
@@ -0,0 +1,20 @@
+package myLearnings;
+import java.lang.Math;
+import java.util.*;
+
+public class Chap3Assin1 {
+
+	public static void main(String []argv)
+	{
+		double Num;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number\n");
+		Num = sc.nextDouble();
+		
+		System.out.println("SquareRoot of number = "+ Math.sqrt(Num));
+		System.out.println("CubeRoot of number = "+Math.cbrt(Num));
+		
+	}
+}
